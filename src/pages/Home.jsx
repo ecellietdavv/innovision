ff440efd852a5e0bbb303367f2e342083db79ad5
Huzaifa-code <huzaifaqureshi037@gmail.com>
2024-06-27 21:00:05 +0530

    First Commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-blue-400 flex justify-center items-center min-h-screen min-w-full text-white'>Home Page of Innovision</div>
+  )
+}
+
+export default Home
